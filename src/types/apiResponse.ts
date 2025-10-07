e357e5bb72adece46069b0a0ca2095bf1a801549
geekyanimesh <animesh.kumar9651@gmail.com>
2025-10-08 00:53:52 +0530

    route for sign-up

--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -1,7 +1,7 @@
 import { Message } from "@/model/User"
 
 export interface ApiResponse{
-    successs: boolean,
+    success: boolean,
     message: string,
     isAcceptingMessages?: boolean,
     messages?: Array<Message>
