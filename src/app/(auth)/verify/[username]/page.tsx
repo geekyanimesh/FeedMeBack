02455f3180a918c756fe1d83b957a83a43189dd3
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-03 15:09:04 +0530

    verify route

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,11 +1,24 @@
+ 'use client';
+
 import { verfiySchema } from '@/schemas/verifySchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useParams } from 'next/navigation';
-import { useRouter } from 'next/router'
+import { useParams, useRouter } from 'next/navigation';
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import * as z from 'zod'
 import axios from 'axios';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'; 
+import { Button } from '@/components/ui/button';
+import { AxiosError } from 'axios';
+import { ApiResponse } from '@/types/apiResponse'; 
+
 
 const VerifyAccount = () => {
     const router = useRouter();
@@ -23,17 +36,44 @@ const VerifyAccount = () => {
                 code: data.code
 
             })
+            //toast
+          router.replace('/sign-in')
         } catch (error) {
-            
+          console.log("Error in signing up user", error);
+          const axiosError = error as AxiosError<ApiResponse>;
+          // toast
         }
 
     }
 
   return (
-    <div>
-      VerifyAccount
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default VerifyAccount
