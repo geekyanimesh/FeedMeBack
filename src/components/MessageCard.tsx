79f8e5a35f2d8ad510f43809856ae1e77883c64c
geekyanimesh <animesh.kumar9651@gmail.com>
2025-11-04 20:23:35 +0530

    adding packages

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import dayjs from 'dayjs';
+import { X } from 'lucide-react';
+import { Message } from '@/model/User';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from './ui/button';
+import { ApiResponse } from '@/types/apiResponse';
+
+const MessageCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MessageCard
